00b0bcb6cc010d47b506c0bc5df61aa391da8b3b
Edgar Moreira <hedegaremoreira@gmail.com>
2024-05-25 22:44:00 +0100

    Hotfix production base url and build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = import.meta.env.PROD;
-
 // https://astro.build/config
-export default defineConfig({
-    base: isProd ? 'blog' : '',
-});
+export default defineConfig({});
